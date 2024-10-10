d939e171e72d581cb9df9009ba48d42a76ca59d7
haivan1212 <haivan@onefin.vn>
2024-10-10 21:32:21 +0700

    update config

--- app/config/config.tsx
+++ app/config/config.tsx
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:8836'
+// const API_URL = 'http://localhost:8836'
+const API_URL = 'https://sit-billpay.onefin.vn'
 const MERCHANT_CODE = '0000001684'
 const MERCHANT_USER_ID = '0918123456'
 // const API_URL = 'http://billpayment-hub.onefin.in'
